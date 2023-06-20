8ac3b36288df9cc75deb3d29290a0a710185dfc6
Adithya Sakaray <adithyasakaray@gmail.com>
2023-06-20 20:32:14 +0530

    minor bug fixes

--- lib/screens/nav_bar.dart
+++ lib/screens/nav_bar.dart
@@ -23,10 +23,7 @@ class _NavBarState extends State<NavBar> {
   }
   final List <Widget> pages = [HomeScreen(),LikeScreen(),ProfileScreen()];
 
-  void initState() {
-    super.initState();
-  }
-
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
